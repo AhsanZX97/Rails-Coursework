de3a60c2ecba585a35db00f04519d62b4fbc2dcb
Ahsan Zia <ahsan97@hotmail.co.uk>
2017-11-15 16:52:05 +0000

    Modified devise pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+
+  devise_scope :user do
+	  root :to => 'devise/sessions#new'
+	  #get "/sign_up" => "devise/sessions#new", as: "new_user_registration"
+  end
+
   devise_for :users
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
