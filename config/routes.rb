ba3c14be090bee14a24b698dcd186cf79cb67e25
Ahsan Zia <az00181@surrey.ac.uk>
2017-12-08 22:02:38 +0000

    added contact page and other fixes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,21 @@
 Rails.application.routes.draw do
 
+  authenticated :user do
+    root "leagues#index"
+  end
+
+  unauthenticated do
+    root 'home#home'
+  end
+
+  get 'contact', to: 'home#contact'
+  post 'request_contact', to: 'home#request_contact'
+
   resources :fixtures
   resources :teams
   resources :leagues
   devise_scope :user do
-	  root :to => 'devise/sessions#new'
+
 	  get "/sign_in" => "leagues#index"
 	  get "/sign_up" => "leagues#index", as: "new_user_registration"
   end
